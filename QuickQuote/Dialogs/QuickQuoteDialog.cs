b1b134f3644cbc02789bd754ce9e16a79c117b31
Janarthanan K <janarthanan.kanagaraj@gmail.com>
2017-07-17 10:10:26 +0530

    Initial Checkin

---
+++ QuickQuote/Dialogs/QuickQuoteDialog.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.FormFlow;
+
+namespace QuickQuote.Dialogs
+{
+    [Serializable]
+    public class QuickQuoteDialog
+    {
+        public static readonly IDialog<string> dialog = Chain.PostToChain()
+            .Select(msg => msg.Text)
+            .Switch(
+            //new RegexCase<IDialog<string>>(new Regex("^hi", RegexOptions.IgnoreCase), (context, text) =>
+            //{
+            //    return Chain.ContinueWith(new GreetingDialog(), AfterGreetingContinuation);
+            //}),
+            new DefaultCase<string, IDialog<string>>((context, text) =>
+            {
+                return Chain.ContinueWith(FormDialog.FromForm(Model.QuickQuote.BuildForm, FormOptions.PromptInStart), AfterGreetingContinuation);
+            }))
+            .Unwrap()
+            .PostToUser();
+
+        private async static Task<IDialog<string>> AfterGreetingContinuation(IBotContext context, IAwaitable<object> item)
+        {
+            var token = await item;
+            var name = "user";
+            context.UserData.TryGetValue("Name", out name);
+            var refNumber = "QTE0012455356";
+            return Chain.Return($"Thanks for using Quick Quote Bot,Premium is £150, Your quote reference number is {refNumber}");
+        }
+
+    }
+}
---
+++ QuickQuote/Model/QuickQuote.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Collections.Generic;
+
+namespace QuickQuote.Model
+{
+    public enum DrivingStyle
+    {
+        LeftHanded,
+        RightHanded
+    }
+
+    public enum NightParking
+    {
+        OnTheRoad,
+        OnAPrivateDriveway,
+        InALockedGarage,
+        InAnUnlockedGarage
+    }
+
+    public enum DayParking
+    {
+        AtHome,
+        WorkPlaceCarParking,
+        OpenPublicCarParking
+    }
+
+    public enum VehicleFittedDevices
+    {
+        Alarm,
+        Immobiliser,
+        TrackingDevice        
+    }
+
+    [Serializable]
+    public class QuickQuote
+    {
+        [Prompt("Please enter your {&}?")]
+        public string FullName { get; set; }
+        [Prompt("Please enter your home {&}")]
+        public string Address { get; set; }
+        [Prompt("Please enter your vehicle Registeration number")]
+        public string RegisterationNumber { get; set; }
+        [Prompt("What is your {&}? {||}")]
+        public DrivingStyle? DrivingStyle { get; set; }
+        [Prompt("Where do you park during night time? {||}")]
+        public NightParking? NightParking { get; set; }
+        [Prompt("Where do you part during day time? {||}")]
+        public DayParking DayParking { get; set; }
+        [Prompt("When did you purchase your vehicle?")]
+        public DateTime VehiclePurchase { get; set; }
+        [Prompt("What is the estimated value of your Car? £")]
+        public Double EstimatedValue { get; set; }
+        [Prompt("What are the safety devices fitted in your vehicle? {||}")]
+        public List<VehicleFittedDevices> VehicleFittedWith { get; set; }
+        [Prompt("Have you been modified your vehicle? {||}")]
+        public bool? VechileModified { get; set; }
+        [Prompt("How many seats your vehicle have?")]
+        public int? NumberOfSeats { get; set; }
+
+        public static IForm<QuickQuote> BuildForm()
+        {
+            return new FormBuilder<QuickQuote>()
+                .Message("Welcome to QuickQuote Bot")
+                .Build();
+        }
+    }
+}
